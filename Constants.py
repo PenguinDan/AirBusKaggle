028d546bdd7d79c05c0de095c190a9e7bbffd169
Daniel Kim <danielkimstudent@hotmail.com>
2018-09-11 22:14:16 -0700

    Delete deeplearning ship detection

---
+++ Constants.py
@@ -0,0 +1,9 @@
+# The directory containing all of the training images
+TRAINING_IMAGE_DIR = 'Training_Data/'
+# The file containing all of the labels for the training data
+TRAINING_LABELS_FILE = 'Training_Data/train_ship_segmentations.csv'
+
+# The directory containing all of the testing images
+TESTING_IMAGE_DIR = 'Test_Data/'
+# The file containing all of the labels for the testing data
+TESTING_lABELS_FILE = 'Testing_Data/test_ship_segmentations.csv'
